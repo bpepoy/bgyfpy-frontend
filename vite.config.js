243bb813be431ac0703569e1af786a16c34299d1
Brian <NCPEPOYDS@GMAIL.COM>
2026-07-26 10:49:20 -0400

    Update PWA icon with black background

--- vite.config.js
+++ vite.config.js
@@ -19,12 +19,12 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
-            src: '/icons/blackgold-logo.png',
+            src: '/icons/blackgold-logo-pwa.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: '/icons/blackgold-logo.png',
+            src: '/icons/blackgold-logo-pwa.png',
             sizes: '512x512',
             type: 'image/png'
           }
